b5b83828292477489fc2515a1626c8ecb4e88079
ZelenovSV <130816124+ZelenovSV@users.noreply.github.com>
2023-04-18 22:32:08 +0300

    Add files via upload

---
+++ Задача 2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+    Console.WriteLine("Введите число №1:");
+    string s1 = Console.ReadLine();
+    int Number1 = int.Parse(s1);
+    Console.WriteLine("Введите число №2:");
+    string s2 = Console.ReadLine();
+    int Number2 = int.Parse(s2);
+
+        if (Number1 > Number2)
+        {
+            Console.WriteLine("Максимальное число: " + Number1);
+            Console.WriteLine("Минимальное число: " + Number2);
+        }
+        else
+        {
+            Console.WriteLine("Максимальное число: " + Number2);
+            Console.WriteLine("Минимальное число: " + Number1);
+        }
